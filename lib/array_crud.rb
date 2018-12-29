5681e4b5743bc809e7dfe4657a38c642402d4b47
Dr. XyClo <sdcarmody@gmail.com>
2018-12-29 22:10:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 def remove_element_from_end_of_array(array)
     array.pop
   end 
-defremove_element_from_end_of_array(array)
+defremove_element_from_start_of_array(array)
 array.shift
 end
 
