bc12e891c3957bbaf3477f56ec4e9948e76aa3c3
Dr. XyClo <sdcarmody@gmail.com>
2018-12-29 22:09:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,8 +10,9 @@ end
 def add_element_to_end_of_array(array, element)
    array.push(element) 
 end
- array = ["howdy", "hi", "hello","hola"]
- element = "aloha"
+
+ 
+ 
 def add_element_to_start_of_array(array, element)
   array.unshift(element) 
 end
