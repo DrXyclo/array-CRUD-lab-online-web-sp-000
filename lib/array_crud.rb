99d7d827c4c30ee7abe2da3b292d5eca3f9f94bb
Dr. XyClo <sdcarmody@gmail.com>
2018-12-29 21:56:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,6 +11,7 @@ def add_element_to_end_of_array(array, element)
   
 end
  array = ["howdy", "hi", "hello","hola"]
+ element = "aloha"
 def add_element_to_start_of_array(array, element)
   array << element 
 end
