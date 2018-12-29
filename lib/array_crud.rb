5c49e50a93bf4a4b1a4ff7821098f263b72cf55b
Dr. XyClo <sdcarmody@gmail.com>
2018-12-29 22:02:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,8 @@ end
 def add_element_to_start_of_array(array, element)
   array.push(element) 
 end
-
+add_element_to_start_of_array(array, element)
+print array
 def remove_element_from_end_of_array(array)
   
 end
